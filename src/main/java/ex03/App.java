af4347effe526ac1ac6555bac20aa1f260e0bbe2
JH <wngml0708@gmail.com>
2025-07-24 11:52:14 +0900

    과제 1차 시도

--- src/main/java/ex03/App.java
+++ src/main/java/ex03/App.java
@@ -3,6 +3,7 @@ package ex03;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
 import java.util.Scanner;
 
 // A 개발자가 만든 것
@@ -18,13 +19,40 @@ public class App {
             Annotation anno = method.getDeclaredAnnotation(RequestMapping.class);
             RequestMapping rm = (RequestMapping) anno;
 
+            // 1. 메서드 찾기
             if (rm.value().equals(path)) {
-                try {
-                    method.invoke(uc);
-                } catch (IllegalAccessException e) {
-                    throw new RuntimeException(e);
-                } catch (InvocationTargetException e) {
-                    throw new RuntimeException(e);
+                // 2. 메서드의 파라미터 여부 확인
+                if (method.getParameterCount() != 0) {
+                    // 파라미터가 있는 경우
+                    Parameter[] parameters = method.getParameters();
+                    for (Parameter parameter : parameters) {
+                        if (parameter.isAnnotationPresent(Principle.class)) {
+                            try {
+                                method.invoke(uc, SessionUser.getInstance());
+                            } catch (IllegalAccessException e) {
+                                throw new RuntimeException(e);
+                            } catch (InvocationTargetException e) {
+                                throw new RuntimeException(e);
+                            }
+                        } else {
+                            try {
+                                method.invoke(uc, Model.getInstance());
+                            } catch (IllegalAccessException e) {
+                                throw new RuntimeException(e);
+                            } catch (InvocationTargetException e) {
+                                throw new RuntimeException(e);
+                            }
+                        }
+                    }
+                } else {
+                    // 파라미터가 없는 경우
+                    try {
+                        method.invoke(uc);
+                    } catch (IllegalAccessException e) {
+                        throw new RuntimeException(e);
+                    } catch (InvocationTargetException e) {
+                        throw new RuntimeException(e);
+                    }
                 }
             }
         }
--- src/main/java/ex03/UserController.java
+++ src/main/java/ex03/UserController.java
@@ -3,8 +3,16 @@ package ex03;
 // B 개발자가 만든 것
 public class UserController {
 
+    @RequestMapping("/userinfo")
+    public void userinfo(@Principle SessionUser sessionUser) {
+        System.out.println(sessionUser.getId());
+        System.out.println(sessionUser.getUsername());
+        System.out.println("userinfo 호출됨");
+    }
+
     @RequestMapping("/login")
-    public void login() {
+    public void login(Model model) {
+        System.out.println(model.getAttribute("username"));
         System.out.println("login 호출됨");
     }
 
